0d8f8cb057e329e2368707e2cb44f83798394d61
Natalia <118902590+Natasha-Trofimova@users.noreply.github.com>
2023-04-14 14:50:44 +0700

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let user = {     // объект
+  name: "John",  // под ключом "name" хранится значение "John"
+  age: 30,       // под ключом "age" хранится значение 30
+};
+alert( user.name ); // John
+alert( user.age ); // 30
