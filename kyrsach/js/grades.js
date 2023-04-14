f6b9cea1eb87a3994fb66678d1ffdeeb577ecfaf
Natalia <118902590+Natasha-Trofimova@users.noreply.github.com>
2023-04-14 14:52:24 +0700

    Add files via upload

---
+++ kyrsach/js/grades.js
@@ -0,0 +1,29 @@
+const GRADES = [
+  {
+    name: 'Овчинникова Ксения Егоровна',
+    className: '9a',
+    informatics: 5,
+    physics: 5,
+    mathemathics: 5,
+    literature: 5,
+    music:  5
+  },
+  {
+    name: 'Мещерякова Кристина Ярославовна',
+    className: '9a',
+    informatics: 3,
+    physics: 4,
+    mathemathics: 3,
+    literature: 5,
+    music: 3
+  },
+  {
+    name: 'Щукина Александра Артёмовна',
+    className: '9a',
+    informatics: 3,
+    physics: 4,
+    mathemathics: 3,
+    literature: 4,
+    music: 3
+  }
+]
